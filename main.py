b7f28ad4aa762aaff343aa3ff06995ed6731957c
jooaoj-tester <128248205+jooaoj-tester@users.noreply.github.com>
2024-05-28 16:03:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 # Assignment: Complete function to return "even" or "odd" depending on given number
 
 def evenOrOdd(x):
-    return "odd" if x % 2 == 0 else "odd"
+    return "even" if x % 2 == 0 else "odd"
